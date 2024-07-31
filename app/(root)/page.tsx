d9fc2eafea68c4eb2c0573f0d89e95eaa102201d
Alan Ng <alan.ng.sp@dlh.de>
2024-07-31 16:00:11 +0200

    Add right side-bar and bank card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,35 @@
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-    const loggedIn = {firstName: 'Alan'}
+  const loggedIn = {
+    firstName: "Alan",
+    lastName: "Ng",
+    email: "alan@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
+        <header className="home-header">
           <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || 'Guest'}
-          subtext="Access or manage your accounts and transactions efficiently."
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access or manage your accounts and transactions efficiently."
           />
-          <TotalBalanceBox accounts={[]} totalBanks={2} totalCurrentBalance={1250.354} />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={2}
+            totalCurrentBalance={1250.354}
+          />
+        </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.54 }, { currentBalance: 546.1234 }]}
+      />
     </section>
   );
 };
